86652912cb13b4b1c1de03558da0d48d7df7ad70
biandeshen <1218480808@qq.com>
2018-03-20 10:52:07 +0800

    完善用户禁用的代码，具体位置参照TODO

---
+++ sandstorm-common/src/main/java/top/sandstorm/common/rest/ForbidException.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: ForbidException
+ * Author:   fanjiangpan
+ * Date:     2018/3/20 10:43
+ * Description: 禁止当前用户登录，如用户可用状态为0
+ * History:
+ * <author>          <time>          <version>
+ * fanjiangpan           2018/3/20           版本号
+ */
+package top.sandstorm.common.rest;
+
+/**
+ * 〈禁止当前用户登录，如用户可用状态为0〉
+ *
+ * @author fanjiangpan
+ * @since 1.0.0
+ */
+public class ForbidException {
+
+}
